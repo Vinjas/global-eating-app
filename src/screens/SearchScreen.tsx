a77249cf93d3f70dea161268709c19d408af529c
Vinyas, Daniel <Daniel.Vinyas@adidas.com>
2022-09-11 00:43:01 +0200

    create base screens and dependencies

---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const SearchScreen = () => {
+
+  return (
+    <View>
+      <Text>Estamos en SearchScreen</Text>
+    </View>
+  )
+}
